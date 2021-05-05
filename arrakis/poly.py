751ead3ddd1a1213f0b9812aee5a2c29ff921da7
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-05-05 23:23:20 +0800

    automatically determining neighboring locations

--- arrakis/poly.py
+++ arrakis/poly.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, LineString
 
 try:
     import importlib.resources as pkg_resources
@@ -38,7 +38,7 @@ def extract():
     return areas
 
 
-def findIntersections(areas):
+def getRegionsLocations(areas):
     regions = []
     locations = []
     for r in areas['polygons'].keys():
@@ -46,6 +46,10 @@ def findIntersections(areas):
             regions.append(r)
         else:
             locations.append(r)
+    return regions, locations
+
+
+def findIntersections(areas, regions, locations):
     locs = {}
     for region in regions:
         region_coords = areas['polygons'][region]
@@ -59,3 +63,20 @@ def findIntersections(areas):
                 locs[location].append(region)
                 locs[location] = list(set(locs[location]))
     return locs
+
+
+def findNeighboring(areas, regions, locations, skip=[]):
+    neighbors = []
+    for i in range(len(locations)):
+        loc1 = locations[i]
+        if loc1 not in skip:
+            loc1_coords = areas['polygons'][loc1]
+            loc1_polygon = Polygon(loc1_coords)
+            for j in range(i+1, len(locations)):
+                loc2 = locations[j]
+                if loc2 not in skip:
+                    loc2_coords = areas['polygons'][loc2]
+                    loc2_polygon = Polygon(loc2_coords)
+                    if loc1_polygon.intersects(loc2_polygon):
+                        neighbors.append(tuple([loc1, loc2]))
+    return neighbors
