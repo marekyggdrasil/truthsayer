f32f6e78734fe8191d813098505fccbb5df74b78
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-05-14 19:43:15 +0800

    saving the prevomputed asset data

--- precompute.py
+++ precompute.py
@@ -1,5 +1,6 @@
-from arrakis.poly import extract, getRegionsLocations, findNeighboring, findIntersections, findStorm
+import json
 
+from arrakis.poly import extract, getRegionsLocations, findNeighboring, findIntersections, findStorm
 
 areas = extract()
 regions, locations = getRegionsLocations(areas)
@@ -7,4 +8,25 @@ location_regions = findIntersections(areas, regions, locations)
 neighbors = findNeighboring(areas, regions, locations, skip=['arrakis', 'tleilaxu_tanks'])
 
 cx, cy, r = findStorm(areas)
-print(cx, cy, r)
+
+filename = 'arrakis/assets/game_config.json'
+
+game_config = None
+with open(filename) as json_file:
+    game_config = json.load(json_file)
+
+game_config['generated'] = {
+    'areas': areas,
+    'regions': regions,
+    'locations': locations,
+    'location_regions': location_regions,
+    'neighbors': neighbors,
+    'map_center': {
+        'x': cx,
+        'y': cy,
+        'r': r
+    }
+}
+
+with open(filename, 'w') as outfile:
+    json.dump(game_config, outfile)
