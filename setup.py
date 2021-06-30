dc8d1248c67a1611ce58ed2c56ddc32612d3e569
marekyggdrasil <marek.yggdrasil@gmail.com>
2021-06-30 22:03:42 +0800

    bumping up the release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='truthsayer',
-    version='0.0.1',
+    version='0.0.2',
     packages=['truthsayer',],
     license='MIT',
     description = 'Visualizes and manages Dune the Board Game state',
